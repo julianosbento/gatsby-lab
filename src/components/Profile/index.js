323055fcd084cb5eb151f3d811fe8d77550ac966
Juliano dos Santos Bento <juliano.bento@pearson.com>
2020-04-02 22:03:59 -0300

    feat: Initial commit with basic structure

---
+++ src/components/Profile/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Profile = () => (
+  <div className="Profile-wrapper">
+    <h1>Juliano Bento</h1>
+    <h2>Full-Stack Developer</h2>
+    <p>Full-Stack Developer at Gauge</p>
+  </div>
+);
+
+export default Profile;
